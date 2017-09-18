4e641f96fd0378993cf1aeb6e80f37bac61f7caa
Robert Pemberton <hellorobertp@gmail.com>
2017-09-18 23:57:25 +0100

    Move Wall component back to App.js

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ const settings = {
 let animationMoveUp = undefined;
 let animationMoveUpAfter = undefined;
 let animationGravity = undefined;
+let animationWall = undefined;
 
 class App extends Component {
   constructor() {
@@ -26,9 +27,12 @@ class App extends Component {
       },
       fallSpeed: 2,
       elevateSpeed: 10,
+      wallX: 400,
+      wallLowY: 300,
     };
   };
   componentDidUpdate() {
+    this.testCollision();
     const fellToGround = this.state.bird.y > settings.appHeight - settings.birdHeight;
 
     if (fellToGround) {
@@ -38,13 +42,9 @@ class App extends Component {
   startGame = () => {
     this.setState({
       gameActive: true,
-      bird: {
-        x: settings.appWidth / 2 - settings.birdWidth / 4,
-        y: 150,
-      },
-      elevateSpeed: 10,
     });
     
+    animationWall = requestAnimationFrame(this.moveWall);
     animationMoveUpAfter = requestAnimationFrame(this.moveUpAfter);
     animationGravity = requestAnimationFrame(this.gravity);
   };
@@ -67,6 +67,10 @@ class App extends Component {
     animationGravity = requestAnimationFrame(this.gravity);
   };
   moveUp = () => {
+    if (!this.state.gameActive) {
+      cancelAnimationFrame(animationMoveUp);
+      return;
+    }
     if (this.state.bird.y > 0) {
       const bird = Object.assign(this.state.bird);
       bird.y = this.state.bird.y - 10;
@@ -77,6 +81,10 @@ class App extends Component {
     }
   };
   moveUpAfter = () => {
+    if (!this.state.gameActive) {
+      cancelAnimationFrame(animationMoveUpAfter);
+      return;
+    }
     if (this.state.bird.y > 0 && this.state.elevateSpeed > 0) {
       const bird = Object.assign(this.state.bird);
       bird.y = this.state.bird.y - this.state.elevateSpeed;
@@ -100,9 +108,16 @@ class App extends Component {
     });
     animationGravity = requestAnimationFrame(this.gravity);
   };
+  testCollision = () => {
+    const heightLowWall = this.state.bird.y + settings.birdHeight >= this.state.wallLowY;
+    const heightHighWall = this.state.bird.y <= this.state.wallLowY - 200;
+    const hitWidth = this.state.bird.x + settings.birdWidth > this.state.wallX && this.state.bird.x < this.state.wallX + 50;
+
+    if ((hitWidth && heightLowWall) || (hitWidth && heightHighWall)) {
+      this.gameOver();
+    }
+  }
   gameOver = () => {
-    cancelAnimationFrame(animationMoveUp);
-    cancelAnimationFrame(animationMoveUpAfter);
     this.setState({
       gameActive: false,
       bird: {
@@ -111,7 +126,26 @@ class App extends Component {
       },
       fallSpeed: 2,
       elevateSpeed: 10,
+      wallX: 400,
+      wallLowY: 300,
     });
+  };
+  moveWall = () => {
+    if (!this.state.gameActive) {
+      cancelAnimationFrame(animationWall);
+      return; 
+    }      
+    if (this.state.wallX > -50) {
+      this.setState({
+        wallX: this.state.wallX - 2,
+      });
+    } else {
+      this.setState({
+        wallX: 400,
+        wallLowY: settings.appHeight - (Math.floor(Math.random() * (300 - 100)) + 100),
+      });
+    }
+    animationWall = requestAnimationFrame(this.moveWall);
   };
   render() {
     return (
@@ -178,8 +212,10 @@ class App extends Component {
             bird={ this.state.bird }
             gameOver={ this.gameOver }
             gameActive={ this.state.gameActive }
+            wallX={ this.state.wallX }
+            wallLowY={ this.state.wallLowY }
           />
-
+          
           { this.state.gameActive &&
             <rect
               width={ settings.birdWidth }
--- src/components/Wall.js
+++ src/components/Wall.js
@@ -1,67 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-// eslint-disable-next-line
-let animationWall = undefined;
+const Wall = (props) => {
+  return(
+    <g>
+      <rect
+        width="50"
+        height={ props.settings.appHeight - (props.settings.appHeight - props.wallLowY) - 200 }
+        x={ props.wallX }
+        y="0"
+        fill="red"
+      />
 
-class Wall extends Component {
-  constructor() {
-    super();
-    this.state = {
-      wallX: 400,
-      wallLowY: 300,
-    }
-  }
-  componentDidMount() {
-    animationWall = requestAnimationFrame(this.moveWall);
-  }
-  componentDidUpdate() {
-    const hitHeight = this.props.bird.y + this.props.settings.birdHeight >= this.state.wallLowY;
-    const hitWidth = this.props.bird.x + this.props.settings.birdWidth > this.state.wallX && this.props.bird.x < this.state.wallX + 50;
-
-    if (hitWidth && hitHeight) {
-      this.props.gameOver();
-    }
-
-    const heightHighWall = this.props.bird.y <= this.state.wallLowY - 200;
-
-    if (hitWidth && heightHighWall) {
-      this.props.gameOver();
-    }
-  }
-  moveWall = () => {
-    if (this.state.wallX > -50) {
-      this.setState({
-          wallX: this.state.wallX - 2,
-      });
-    } else {
-      this.setState({
-        wallX: 400,
-        wallLowY: this.props.settings.appHeight - (Math.floor(Math.random() * (300 - 100)) + 100),
-      });
-    }
-    animationWall = requestAnimationFrame(this.moveWall);
-  };
-  render() {
-    return(
-      <g>
-        <rect
-          width="50"
-          height={ this.props.settings.appHeight - (this.props.settings.appHeight - this.state.wallLowY) - 200 }
-          x={ this.state.wallX }
-          y="0"
-          fill="red"
-        />
-
-        <rect
-          width="50"
-          height={ this.props.settings.appHeight - this.state.wallLowY }
-          x={ this.state.wallX }
-          y={ this.state.wallLowY }
-          fill="red"
-        />
-      </g>
-    )
-  }
+      <rect
+        width="50"
+        height={ props.settings.appHeight - props.wallLowY }
+        x={ props.wallX }
+        y={ props.wallLowY }
+        fill="red"
+      />
+    </g>
+  )
 }
 
 export default Wall;
